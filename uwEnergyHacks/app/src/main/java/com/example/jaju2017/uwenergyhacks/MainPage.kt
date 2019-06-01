516b3a203dbf0eaede0bebe2ffcd7e82456ebf8f
Jaeho Jung <jaju2017@v1040-wn-rt-c-65-86.campus-dynamic.uwaterloo.ca>
2019-06-01 18:29:35 -0400

    initial commit

---
+++ uwEnergyHacks/app/src/main/java/com/example/jaju2017/uwenergyhacks/MainPage.kt
@@ -0,0 +1,12 @@
+package com.example.jaju2017.uwenergyhacks
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_page)
+    }
+}
